5a4668cd9e00888d3815fe51d596c26c255f67f8
roberto286 <roberto.saliola96@gmail.com>
2026-06-16 17:43:31 +0200

    feat: add textnode to htmlnode function with unit tests

--- src/nodes/textnode.py
+++ src/nodes/textnode.py
@@ -1,5 +1,7 @@
+from dataclasses import dataclass
 from enum import Enum
-from typing_extensions import override
+
+from nodes.leafnode import LeafNode
 
 
 class TextType(Enum):
@@ -8,29 +10,37 @@ class TextType(Enum):
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
+    TEXT = "text"
+    ITALIC = "italic"
 
 
+@dataclass
 class TextNode:
     text: str
     text_type: TextType
-    url: str | None
+    url: str | None = None
 
-    def __init__(self, text: str, text_type: TextType, url: str | None = None):
-        self.text = text
-        self.text_type = text_type
-        self.url = url
 
-    @override
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, TextNode):
-            return False
+def text_node_to_html_node(text_node: "TextNode"):
+    text_type = text_node.text_type
 
-        return (
-            self.text == other.text
-            and self.text_type == other.text_type
-            and self.url == other.url
+    if text_type == TextType.TEXT:
+        return LeafNode(tag=None, value=text_node.text)
+    if text_type == TextType.BOLD:
+        return LeafNode(tag="b", value=text_node.text)
+    if text_type == TextType.ITALIC:
+        return LeafNode(tag="i", value=text_node.text)
+    if text_type == TextType.CODE:
+        return LeafNode(tag="code", value=text_node.text)
+    if text_type == TextType.LINK:
+        return LeafNode(
+            tag="a", props={"href": text_node.url or ""}, value=text_node.text
+        )
+    if text_type == TextType.IMAGE:
+        return LeafNode(
+            tag="img",
+            value="",
+            props={"src": text_node.url or "", "alt": text_node.text},
         )
 
-    @override
-    def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    raise Exception()
